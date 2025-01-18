71ee76c19340c246acd94ca1bcb3d9b2739b3e9e
brimoutain <1186521289@qq.com>
2025-01-18 16:28:58 +0800

    111

---
+++ Assets/Scripts/Input/MakeInput.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Android;
+using UnityEngine.InputSystem;
+
+public class MakeInput : MonoBehaviour
+{
+    private MakeInput makeInput;
+
+    [SerializeField]private float maxAir;
+    private float currentAir;
+    [SerializeField] private float proportion;//时间与空气之比
+    private float time;
+    private bool isInhaled;
+    [SerializeField] private GameObject sprite;//两个半圆弧？
+
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private GameObject tool;
+    private float distance;
+    [SerializeField] private GameObject mouth;
+
+    private PrepareInput lastInput;
+
+    void Start()
+    {
+        makeInput = GetComponent<MakeInput>();
+        lastInput =GameObject.Find("PrepareInput").GetComponent<PrepareInput>();
+        isInhaled = false;
+    }
+    void Update()
+    {
+        if (!isInhaled) InhaleRecord();//用ishaled来判断是否进入吹气
+        else if (isInhaled)
+        {
+            BlowControl();
+            ToolControl();
+        }
+    }
+
+    void InhaleRecord()//记录吸气阶段的空气量
+    {
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            time += Time.deltaTime;
+            currentAir = time /proportion;
+
+            if (currentAir >= maxAir)
+            {//强制退出吸气
+                isInhaled = true;
+                time = 0;
+            }
+
+        }else if (Keyboard.current.spaceKey.wasReleasedThisFrame)//松开空格退出吹气
+        {
+            isInhaled = true;
+            time = 0;
+        }
+    }
+
+    void BlowControl()
+    {
+        if (Keyboard.current.spaceKey.isPressed)
+        {
+            time += Time.deltaTime;
+            currentAir -= time /proportion;
+            if(currentAir <= 0)//没气就不准吹了
+            {
+                makeInput.enabled = false;
+            }
+        }
+    }
+
+    void ToolControl()
+    {
+        
+        distance = Vector2.Distance(tool.transform.position, mouth.transform.position);
+        if(Keyboard.current.wKey.isPressed)
+        {
+            tool.transform.position = new Vector3(tool.transform.position.x, tool.transform.position.y + moveSpeed, 0);
+            tool.transform.localScale *= 0.995f;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            tool.transform.position = new Vector3(tool.transform.position.x, tool.transform.position.y - moveSpeed, 0);
+            tool.transform.localScale *= 1.005f;
+        }
+    }//控制工具移动并计算距离
+
+    void SpriteScaled()//准备写半圆弧缩放，变化速度直接用time操控，和proportion分离开
+    {
+
+    }
+}
---
+++ Assets/Scripts/Input/PrepareInput.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PrepareInput : MonoBehaviour
+{
+    private PrepareInput prepareInput;
+
+    [SerializeField]private Text text;
+    public int numOfMaterial {  get; private set; }
+    [SerializeField] protected RectTransform Square;//将蘸取量体现在长条里
+
+    private float[] ratio = new float[3];//记录每个材料的含量
+
+    public float sumoftime;
+    private float time;
+    private float sum;
+
+    [SerializeField] private GameObject water0;//不同材料及其编号
+    [SerializeField] private GameObject ganyou1;
+    [SerializeField] private GameObject xijiejing2;
+    private GameObject[] obj = new GameObject[3];
+
+    void Start()
+    {
+        prepareInput = GetComponent<PrepareInput>();//获取玩家键盘输入
+        numOfMaterial = 0;//通过0-2控制蘸取的液体
+        prepareInput.enabled = true;
+        for(int i = 0; i < 3; i++)
+        {
+            switch(i)
+            {
+                case 0:
+                    obj[i] = water0;
+                    break;
+                case 1:
+                    obj[i] = ganyou1;
+                    break;
+                case 2:
+                    obj[i]= xijiejing2;
+                    break;
+            }
+        }
+        BottleChoose(obj[numOfMaterial]);
+    }
+    void Update()
+    {
+        CheckInput();
+        ChangeScene();
+    }
+
+    void CheckNum()
+    {
+        if (numOfMaterial < 0) numOfMaterial = 2;
+        if (numOfMaterial > 2) numOfMaterial = 0;
+        BottleChoose(obj[numOfMaterial]);
+    }//检测数字处于0-2之间
+
+    void CheckInput()
+    {
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            RestorePosition(obj[numOfMaterial]);
+            numOfMaterial--;
+            CheckNum();
+        }
+        else if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            RestorePosition(obj[numOfMaterial]);
+            numOfMaterial++;
+            CheckNum();
+        }
+
+        if (Keyboard.current.jKey.isPressed)
+        {
+            time += Time.deltaTime;
+            sum = (time / sumoftime) * 100;
+            Square.sizeDelta = new Vector2(sum*2, 20);
+
+            text.text = ((int)sum).ToString() + "/100";
+
+            if (time >= sumoftime)
+                prepareInput.enabled = false;
+
+        }else if (Keyboard.current.jKey.wasReleasedThisFrame)
+        {
+            ratio[numOfMaterial] = sum;
+            for(int i = 0; i < 2; i++)
+            {
+                if(i==numOfMaterial)
+                    continue;
+                else if (i != numOfMaterial)
+                {
+                    ratio[numOfMaterial]-=ratio[i];
+                }
+            }
+        }
+    }//对输入进行判断，包括ad与j
+
+    void ChangeScene()
+    {
+        if(sum>=100)
+        {
+            SceneManager.LoadScene("Make");
+            DontDestroyOnLoad(prepareInput);
+        }
+    }
+
+    void BottleChoose(GameObject obj)
+    {
+        obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y + 3, 0);
+    }
+
+    void RestorePosition(GameObject obj)
+    {
+        obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y - 3, 0);
+    }
+
+}
